54e5f81a5f0f20d50f37ab0767ba228fb825447d
Parkjju <rudwns3927@gmail.com>
2023-05-22 20:57:40 +0900

    feat: UIView 확장 / MVC View 기준 부모 뷰 컨트롤러 레퍼런스 시 사용

--- runnerpia/Extensions/UIView.swift
+++ runnerpia/Extensions/UIView.swift
@@ -5,4 +5,19 @@
 //  Created by Jun on 2023/05/22.
 //
 
-import Foundation
+import UIKit
+
+// MARK: UIView에서 parent ViewController를 찾아주는 확장 함수
+extension UIView {
+    var parentViewController: UIViewController? {
+        // Starts from next (As we know self is not a UIViewController).
+        var parentResponder: UIResponder? = self.next
+        while parentResponder != nil {
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+            parentResponder = parentResponder?.next
+        }
+        return nil
+    }
+}
