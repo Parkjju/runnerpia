7f59696affa564734f2d5ead233825d4ac314fe0
Parkjju <rudwns3927@gmail.com>
2023-05-22 23:30:44 +0900

    fix: Route 모델 프로퍼티 routeName 추가

--- runnerpia/Models/Route.swift
+++ runnerpia/Models/Route.swift
@@ -10,6 +10,7 @@ import CoreLocation
 
 struct Route{
     let user: User?
+    let routeName: String?
     let arrayOfPos: [CLLocationCoordinate2D]?
     let runningTime: String?
     let review: String?
