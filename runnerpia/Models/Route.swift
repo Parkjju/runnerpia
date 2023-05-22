e3eb11478e4ca974ef271254a4cdc6af5921bd53
Parkjju <rudwns3927@gmail.com>
2023-05-23 00:08:21 +0900

    feat: Route모델 수정 - distance 프로퍼티 추가

--- runnerpia/Models/Route.swift
+++ runnerpia/Models/Route.swift
@@ -11,6 +11,7 @@ import CoreLocation
 struct Route{
     let user: User?
     let routeName: String?
+    let distance: Int?
     let arrayOfPos: [CLLocationCoordinate2D]?
     let runningTime: String?
     let review: String?
