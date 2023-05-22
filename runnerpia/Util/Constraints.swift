b1bdab552bb153a8b6fa354adf1591389a4e265f
Parkjju <rudwns3927@gmail.com>
2023-05-22 23:31:09 +0900

    feat: 재사용 제약조건 수치 그룹화

---
+++ runnerpia/Util/Constraints.swift
@@ -0,0 +1,13 @@
+//
+//  Constraints.swift
+//  runnerpia
+//
+//  Created by Jun on 2023/05/22.
+//
+
+import Foundation
+
+struct Constraints{
+    static let paddingLeftAndRight = 16
+    static let fontSize = 20
+}
