ab039da050dabff7fc56e71b99eec04a1b9cea12
Parkjju <rudwns3927@gmail.com>
2023-05-23 09:49:50 +0900

    feat: 경로 리스트 뷰 제작

--- runnerpia/Views/Recommend/List/RecommendTableViewCell.swift
+++ runnerpia/Views/Recommend/List/RecommendTableViewCell.swift
@@ -18,6 +18,7 @@ class RecommendTableViewCell: UITableViewCell {
     
     let mainLabel: UILabel = {
         let label = UILabel()
+        label.lineBreakMode = .byTruncatingTail
         return label
     }()
     
@@ -51,10 +52,11 @@ class RecommendTableViewCell: UITableViewCell {
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 2
         
-        let text = NSMutableAttributedString(string: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 12), NSAttributedString.Key.paragraphStyle: paragraphStyle])
+        let text = NSMutableAttributedString(string: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .light, width: .standard), NSAttributedString.Key.paragraphStyle: paragraphStyle])
         
         label.attributedText = text
-        label.numberOfLines = 0
+        label.numberOfLines = 2
+        label.lineBreakMode = .byTruncatingTail
         
         return label
     }()
@@ -62,7 +64,10 @@ class RecommendTableViewCell: UITableViewCell {
     let tagBox: UIStackView = {
         let sv = UIStackView()
         sv.axis = .horizontal
+        sv.spacing = 100
         sv.distribution = .fillProportionally
+        sv.alignment = .fill
+        sv.backgroundColor = .black
         return sv
     }()
     
@@ -93,31 +98,79 @@ class RecommendTableViewCell: UITableViewCell {
     }
     
     func setupUI(){
+        setContentView()
+        setUIWithData()
+    }
+    
+    func setContentView(){
         self.backgroundColor = .clear
         self.selectionStyle = .none
         
         contentView.backgroundColor = .white
         contentView.layer.cornerRadius = 10
         contentView.layer.masksToBounds = true
-        
+    }
+    func setUIWithData(){
         guard let data = cellData else {
             return
         }
         
         mainLabel.text = data.routeName
         infoLabel.text = "\(data.runningDate!) ﹒ \(data.runningTime!) ﹒ \(data.distance!)km"
+        summaryLabel.text = "\(data.review!)"
+        
+        setMap(data)
+        setTags(secureTags: data.secureTags!, recommendedTags: data.recommendedTags!)
+    }
+    
+    func setMap(_ data: Route){
+        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: data.arrayOfPos!.first!.latitude, lng: data.arrayOfPos!.first!.longitude))
+        map.moveCamera(cameraUpdate)
+        map.minZoomLevel = 10
+        map.maxZoomLevel = 20
+        
+        let pathOverlay = NMFPath()
+        let points:[NMGLatLng] = data.arrayOfPos!.map {NMGLatLng(from:$0)}
+        pathOverlay.path = NMGLineString(points: points)
+        pathOverlay.color = .polylineColor
+        pathOverlay.outlineColor = .clear
+        pathOverlay.width = 10
+        pathOverlay.mapView = map
+    }
+    
+    func setTags(secureTags: [String], recommendedTags: [String]){
+        let secureTagBox = secureTags.map {
+            let tag = Tag()
+            tag.isSecureTag = true
+            tag.tagName = globalSecureTags[Int($0)!]
+            return tag
+        }
+        
+        let recommendedTagBox = recommendedTags.map{
+            let tag = Tag()
+            tag.isSecureTag = false
+            tag.tagName = globalRecommendedTags[Int($0)!]
+            return tag
+        }
+        
+        let gradientTag = Tag()
+        gradientTag.isGradient = true
+        gradientTag.tagName = "+3"
+        
+        [secureTagBox.first!, recommendedTagBox.first!, gradientTag].forEach { tagBox.addSubview($0) }
     }
 }
 
 extension RecommendTableViewCell: LayoutProtocol{
     func setSubViews() {
-        [mainLabel,button, infoLabel].forEach { self.contentView.addSubview($0) }
+        [mainLabel,button, infoLabel, map, summaryLabel, tagBox].forEach { self.contentView.addSubview($0) }
     }
     
     func setLayout() {
         mainLabel.snp.makeConstraints {
             $0.top.equalTo(self.contentView.snp.top).offset(16)
             $0.leading.equalTo(self.contentView.snp.leading).offset(16)
+            $0.trailing.equalTo(button.snp.leading)
         }
         
         button.snp.makeConstraints {
@@ -129,6 +182,26 @@ extension RecommendTableViewCell: LayoutProtocol{
         infoLabel.snp.makeConstraints {
             $0.top.equalTo(mainLabel.snp.bottom).offset(6)
             $0.leading.equalTo(mainLabel.snp.leading)
+        }
+        
+        map.snp.makeConstraints {
+            $0.leading.equalTo(mainLabel.snp.leading)
+            $0.trailing.equalTo(self.contentView.snp.trailing).offset(-16)
+            $0.top.equalTo(infoLabel.snp.bottom).offset(10)
+            $0.height.equalTo(160)
+        }
+        
+        summaryLabel.snp.makeConstraints {
+            $0.leading.equalTo(mainLabel.snp.leading)
+            $0.trailing.equalTo(self.contentView.snp.trailing).offset(-16)
+            $0.top.equalTo(map.snp.bottom).offset(10)
+        }
+        
+        tagBox.snp.makeConstraints {
+            $0.leading.equalTo(mainLabel.snp.leading)
+            $0.trailing.equalTo(self.contentView.snp.trailing).offset(-16)
+            $0.top.equalTo(summaryLabel.snp.bottom).offset(10)
+            $0.bottom.equalTo(self.contentView.snp.bottom).offset(-20)
         }
     }
 }
--- runnerpia/Views/Recommend/List/RecommendView.swift
+++ runnerpia/Views/Recommend/List/RecommendView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class RecommendView: UIView {
     // MARK: Properties
@@ -62,11 +63,13 @@ class RecommendView: UIView {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.separatorStyle = .none
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 220
     }
-    
+
     private func setupData(){
-        let firstData = Route(user: User(userId: "경준", nickname: "경준"), routeName: "한강 잠실 러닝길" ,distance: 500, arrayOfPos: nil, runningTime: "500분", review: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", runningDate: "12월 31일 토요일 오후 6~9시", recommendedTags: ["1","2"], secureTags: ["0","1","2"], files: nil)
-        let secondData = Route(user: User(userId: "경준", nickname: "경준"),routeName: "한강 잠실 러닝길",distance: 300,arrayOfPos: nil, runningTime: "320분", review: "예시 데이터입니다. 여기 러닝 코스 아주 괜찮습니다. 붕어빵 가게도 있습니다. 중간에 ㅕ편의점도 있어요 ! 경치가 좋아요~ 고양이가 많습니당", runningDate: "12월 30일 일요일 오후 2~3시", recommendedTags: ["1","2","3"], secureTags: ["1","2"], files: nil)
+        let firstData = Route(user: User(userId: "경준", nickname: "경준"), routeName: "한강 잠실 러닝길한강 잠실 러닝길한강 잠실 러닝길" ,distance: 500, arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2785, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2779, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2767, longitude: 127.1444)], runningTime: "500분", review: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", runningDate: "12월 31일 토요일 오후 6~9시", recommendedTags: ["1","2"], secureTags: ["0","1","2"], files: nil)
+        let secondData = Route(user: User(userId: "경준", nickname: "경준"),routeName: "한강 잠실 러닝길",distance: 300,arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2759, longitude: 127.1488), CLLocationCoordinate2D(latitude: 37.2765, longitude: 127.1493), CLLocationCoordinate2D(latitude: 37.2771, longitude: 127.1502)], runningTime: "320분", review: "예시 데이터입니다. 여기 러닝 코스 아주 괜찮습니다. 붕어빵 가게도 있습니다. 중간에 ㅕ편의점도 있어요 ! 경치가 좋아요~ 고양이가 많습니당", runningDate: "12월 30일 일요일 오후 2~3시", recommendedTags: ["0","1","2"], secureTags: ["0","1"], files: nil)
         
         
         routeData.append(firstData)
@@ -90,20 +93,12 @@ extension RecommendView: LayoutProtocol{
             $0.top.equalTo(mainLabel.snp.bottom).offset(20)
             $0.leading.equalToSuperview().offset(Constraints.paddingLeftAndRight)
             $0.trailing.equalToSuperview().offset(-Constraints.paddingLeftAndRight)
-            $0.bottom.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(-100)
         }
     }
 }
 
 extension RecommendView: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 220
-    }
-    
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
-    }
-    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
         headerView.backgroundColor = .clear
