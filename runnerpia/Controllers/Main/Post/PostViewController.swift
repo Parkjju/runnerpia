1b28cb8e178bb35e0a669e35b6896dc0dde8eba2
Parkjju <rudwns3927@gmail.com>
2023-05-24 20:48:43 +0900

    feat: PostViewController 생성 및 뷰 교체

---
+++ runnerpia/Controllers/Main/Post/PostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostViewController.swift
+//  runnerpia
+//
+//  Created by Jun on 2023/05/24.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let postView = PostView()
+        self.view = postView
+    }
+
+}
