b0ea856f2fa60163fd4dea312330762fb0815e26
Parkjju <rudwns3927@gmail.com>
2023-05-29 20:20:22 +0900

    feat: Tag 열거형 정의

--- runnerpia/Util/Tags.swift
+++ runnerpia/Util/Tags.swift
@@ -10,17 +10,18 @@ import Foundation
 let globalRecommendedTags: [String] = ["안심등이 있어요","늦은 시간까지 사람이 많아요","강을 따라 달려요","가파른 구간이 없어요"]
 let globalSecureTags: [String] = ["안심등이 있어요", "늦은 시간까지 사람이 많아요", "강을 따라 달려요", "가파른 구간이 없어요", "울창한 나무숲"]
 
-enum GlobalTags{
-    case safety
-    case night
+enum NormalTags{
     case river
-    case uphill
+    case woody
+    case clean
+    case flat
+    case walkerTrack
 }
 
 enum SecureTags{
-    case safety
-    case night
-    case river
-    case uphill
     case schoolZone
+    case safetyPole
+    case streetLight
+    case manyPeopleInNight
+    case manyPeopleInNoon
 }
