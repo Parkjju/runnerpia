e8702444138f526446636526d685ca9f6fc3ca67
Parkjju <rudwns3927@gmail.com>
2023-05-29 14:27:35 +0900

    feat: 모델정의

--- runnerpia/Util/Tags.swift
+++ runnerpia/Util/Tags.swift
@@ -6,6 +6,21 @@
 //
 
 import Foundation
-
+// enum refactoring
 let globalRecommendedTags: [String] = ["안심등이 있어요","늦은 시간까지 사람이 많아요","강을 따라 달려요","가파른 구간이 없어요"]
 let globalSecureTags: [String] = ["안심등이 있어요", "늦은 시간까지 사람이 많아요", "강을 따라 달려요", "가파른 구간이 없어요", "울창한 나무숲"]
+
+enum GlobalTags{
+    case safety
+    case night
+    case river
+    case uphill
+}
+
+enum SecureTags{
+    case safety
+    case night
+    case river
+    case uphill
+    case schoolZone
+}
