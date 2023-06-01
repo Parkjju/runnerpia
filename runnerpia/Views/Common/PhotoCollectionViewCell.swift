920e01e91d38d77fd58a6f4e569837ced2f57149
Parkjju <rudwns3927@gmail.com>
2023-06-01 21:49:04 +0900

    feat: 포토 컬렉션뷰 델리게이트 추가 / 이미지 삭제 버튼 addTarget 등록 / prepareForReuse 함수 오버라이딩 후 컬렉션뷰 초기화 관련 로직 추가 / 컬렉션뷰 border 속성 부여 / 이미지 사이즈 명시적 선언 및 center 오토레이아웃 정렬 (이미지버튼 상위로 끌어올리기) / 레이아웃 서브뷰 계층 조절

--- runnerpia/Views/Common/PhotoCollectionViewCell.swift
+++ runnerpia/Views/Common/PhotoCollectionViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class PhotoCollectionViewCell: UICollectionViewCell {
+    var delegate: PhotoCollectionViewCellEventDelegate?
     var selectedImage: UIImage?{
         didSet{
             setupUI()
@@ -41,6 +42,8 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         btn.backgroundColor = .white
         btn.layer.borderColor = UIColor.grey300.cgColor
         btn.layer.borderWidth = 1
+        
+        btn.addTarget(self, action: #selector(removeButtonTapped), for: .touchUpInside)
         return btn
     }()
     
@@ -63,17 +66,29 @@ class PhotoCollectionViewCell: UICollectionViewCell {
     override func didMoveToSuperview() {
         setLayout()
         setupUI()
+        setupDelegate()
     }
     
     // MARK: 초기화 후 추가버튼 UI를 어떻게 재조정할지
     override func prepareForReuse() {
         super.prepareForReuse()
         imageView.image = nil
+        removeButton.isHidden = false
     }
+    
+    // MARK: Selectors
+    @objc func removeButtonTapped(){
+        // 버튼은 달려있는데 안보이
+        delegate?.removeButtonTapped(self)
+    }
+    
+    // MARK: Helpers
     
     func setupUI(){
         self.clipsToBounds = true
         self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.grey100.cgColor
         
         // MARK: addImage 셀 UI오류 예상후보 1. Constraints 중복 추가 -> 아님
         if(selectedImage!.isSymbolImage){
@@ -81,6 +96,7 @@ class PhotoCollectionViewCell: UICollectionViewCell {
             
             // MARK: 예상후보 2. 재사용 과정에서 imageView 인스턴스가 addImage 위를 덮음 -> 정답!
             imageView.snp.removeConstraints()
+            removeButton.isHidden = true
             self.contentView.backgroundColor = .grey100
             
             self.addImage.snp.makeConstraints {
@@ -90,30 +106,39 @@ class PhotoCollectionViewCell: UICollectionViewCell {
                 $0.height.equalTo(20)
             }
         }else{
+            // MARK: 이미지뷰 사이즈에 대한 명시적 선언
             self.contentView.backgroundColor = .black
+            
             imageView.snp.makeConstraints {
-                $0.top.equalTo(self.snp.top)
-                $0.leading.equalTo(self.snp.leading)
-                $0.trailing.equalTo(self.snp.trailing)
-                $0.bottom.equalTo(self.snp.bottom)
+                $0.centerY.equalTo(self.contentView.snp.centerY)
+                $0.centerX.equalTo(self.contentView.snp.centerX)
+                $0.width.equalTo(self.contentView.frame.width)
+                $0.height.equalTo(self.contentView.frame.height)
             }
             imageView.image = selectedImage
-            
         }
+    }
+    
+    func setupDelegate(){
+        self.delegate = self.parentViewController as! PostDetailViewController
     }
 }
 
 extension PhotoCollectionViewCell: LayoutProtocol{
     func setSubViews() {
-        [addImage, removeButton].forEach { self.contentView.addSubview($0) }
-        [imageView].forEach { self.addSubview($0) }
+        [addImage,imageView, removeButton].forEach { self.contentView.addSubview($0) }
     }
     
     // MARK: estimated size를 통해 자동 재계산 되고있는지 확인 필요
+    // MARK: 삭제버튼 삭제하면 컨텐츠뷰 사이즈 조절이 안됨
     func setLayout() {
         removeButton.snp.makeConstraints {
             $0.trailing.equalTo(self.contentView.snp.trailing).offset(-5)
             $0.top.equalTo(self.contentView.snp.top).offset(5)
         }
     }
+}
+
+protocol PhotoCollectionViewCellEventDelegate{
+    func removeButtonTapped(_ selectedCell: PhotoCollectionViewCell)
 }
