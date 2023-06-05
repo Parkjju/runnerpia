ada66822a9c936cf6be281136b30165ba0021258
Parkjju <rudwns3927@gmail.com>
2023-06-05 16:46:16 +0900

    feat: Route 테스트용 인스턴스 데이터 수정 (사진추가)

--- runnerpia/Views/Recommend/List/RecommendView.swift
+++ runnerpia/Views/Recommend/List/RecommendView.swift
@@ -68,12 +68,15 @@ class RecommendView: UIView {
     }
 
     private func setupData(){
-        let firstData = Route(user: User(userId: "경준", nickname: "경준"), routeName: "한강 잠실 러닝길한강 잠실 러닝길한강 잠실 러닝길" ,distance: 500, arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2785, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2779, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2767, longitude: 127.1444)], runningTime: "500분", review: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", runningDate: "12월 31일 토요일 오후 6~9시", recommendedTags: ["1","2"], secureTags: ["0","1","2"], files: nil)
-        let secondData = Route(user: User(userId: "경준", nickname: "경준"),routeName: "한강 잠실 러닝길",distance: 300,arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2759, longitude: 127.1488), CLLocationCoordinate2D(latitude: 37.2765, longitude: 127.1493), CLLocationCoordinate2D(latitude: 37.2771, longitude: 127.1502)], runningTime: "320분", review: "예시 데이터입니다. 여기 러닝 코스 아주 괜찮습니다. 붕어빵 가게도 있습니다. 중간에 ㅕ편의점도 있어요 ! 경치가 좋아요~ 고양이가 많습니당", runningDate: "12월 30일 일요일 오후 2~3시", recommendedTags: ["0","1","2"], secureTags: ["0","1"], files: nil)
+        let firstData = Route(user: User(userId: "경준", nickname: "경준"), routeName: "한강 잠실 러닝길한강 잠실 러닝길한강 잠실 러닝길" ,distance: 500, arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2785, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2779, longitude: 127.1452),CLLocationCoordinate2D(latitude: 37.2767, longitude: 127.1444)], location:"성동구 송정동" , runningTime: "500분", review: "두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.두줄까지 작성됩니다. 두줄 넘어가면 좌/우 여백 유지하면서 좌측 정렬로 줄내림되면서 ... 처리 됩니다.", runningDate: "12월 31일 토요일 오후 6~9시", recommendedTags: ["1","2"], secureTags: ["0","1","2"], files: nil)
+        let secondData = Route(user: User(userId: "경준", nickname: "경준"),routeName: "한강 잠실 러닝길",distance: 300,arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2759, longitude: 127.1488), CLLocationCoordinate2D(latitude: 37.2765, longitude: 127.1493), CLLocationCoordinate2D(latitude: 37.2771, longitude: 127.1502)], location:"기흥구 동백동" , runningTime: "320분", review: "예시 데이터입니다. 여기 러닝 코스 아주 괜찮습니다. 붕어빵 가게도 있습니다. 중간에 ㅕ편의점도 있어요 ! 경치가 좋아요~ 고양이가 많습니당", runningDate: "12월 30일 일요일 오후 2~3시", recommendedTags: ["0","1","2"], secureTags: ["0","1"], files: nil)
+        
+        let thirdData = Route(user: User(userId: "경준", nickname: "경준"),routeName: "동백 호수공원",distance: 200,arrayOfPos: [CLLocationCoordinate2D(latitude: 37.2709, longitude: 127.1488), CLLocationCoordinate2D(latitude: 37.2765, longitude: 127.1493), CLLocationCoordinate2D(latitude: 37.2771, longitude: 127.1502)], location:"기흥구 언동로" , runningTime: "200분", review: "나무가 풍성한 동백 호수공원을 달려보아요", runningDate: "12월 30일 일요일 오후 2~3시", recommendedTags: ["0","1","2"], secureTags: ["0","1", "2", "3", "4"], files:[UIImage(named: "test1")!, UIImage(named:"test2")!, UIImage(named:"test3")!, UIImage(named:"test4")!])
         
         
         routeData.append(firstData)
         routeData.append(secondData)
+        routeData.append(thirdData)
     }
 
 }
