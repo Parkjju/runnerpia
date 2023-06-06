b909c61537389b08a1b75508e17a263da31e82df
Parkjju <rudwns3927@gmail.com>
2023-06-06 23:36:23 +0900

    feat: recommendVC 메모리 해제 로직 개선 / removeAll vs removeSubrange 관련 주석처리

--- runnerpia/Controllers/TabBarController.swift
+++ runnerpia/Controllers/TabBarController.swift
@@ -18,34 +18,24 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         self.delegate = self
     }
     
-    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        print("hi")
-    }
-    
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         let selectedNavigationVC = viewController as! UINavigationController
+        let recommendNavigationVC = tabBarController.viewControllers?.first as! UINavigationController
         
         // MARK: 메모리상에 RecommendVC가 살아있으면 해제
         guard let _ = selectedNavigationVC.viewControllers.first as? RecommendViewController else {
             
-            if(selectedNavigationVC.viewControllers.count == 0){
-                selectedNavigationVC.pushViewController(RecommendViewController(), animated: false)
+            if(recommendNavigationVC.viewControllers.count == 0){
+                recommendNavigationVC.pushViewController(RecommendViewController(), animated: false)
                 return
             }else{
-                
-                let tabbarFirstNavigationVC = self.viewControllers!.first as! UINavigationController
-                tabbarFirstNavigationVC.viewControllers.removeAll()
+                // MARK: removeAll VS removeSubrange로 테이블뷰 목록은 살려둘지
+                // MARK: 다른 뷰들 통신하는 과정에서 성능 저하가 심해지면 removeAll로 모든 지도 데이터들을 삭제해줘야됨
+                recommendNavigationVC.viewControllers.removeAll()
+//                recommendNavigationVC.viewControllers.removeSubrange(1..<recommendNavigationVC.viewControllers.count)
                 return
             }
         }
-        
-        
-        // MARK: 탭바 셀렉트 뷰컨이 경로 따라가기 Navigation VC면 RecommendVC 푸시
-        
-        
-        
-    
-        
     }
 
 }
