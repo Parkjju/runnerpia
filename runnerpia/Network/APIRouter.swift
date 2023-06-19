29bb4bf572c7f3953e1bedf91f15d65fc83ec6cd
Parkjju <rudwns3927@gmail.com>
2023-06-19 13:11:21 +0900

    feat: route API 추가

--- runnerpia/Network/APIRouter.swift
+++ runnerpia/Network/APIRouter.swift
@@ -130,3 +130,88 @@ enum UserEndPoint: APIConfiguration{
 }
 
 // MARK: RouteEndpoint
+enum RouteEndPoint: APIConfiguration{
+    case postRoute(accessToken: String, route: Route)
+    case getRoute(accessToken: String, id: Int)
+    case getReview(accessToken: String, id: Int)
+    case modifyRoute(accessToken: String, id: Int)
+    case deleteRoute(accessToken: String, id: Int)
+    case checkIsRun(accessToken: String, id: Int)
+    case getAllRoute(accessToken: String)
+    case getAllReview(accessToken: String)
+    case getRouteFromCoordinate(accessToken: String, longitude: CGFloat, latitude: CGFloat)
+    case getRouteFromCityName(accessToken: String, city: String, state: String)
+    case checkRouteNameIsDuplicated(accessToken: String, routeName: String)
+    case getRecommendedRouteFromCoordinate(accessToken: String, longitude: CGFloat, latitude: CGFloat)
+    case getPopularTags(accessToken: String)
+    
+    var method: Alamofire.HTTPMethod{
+        switch self{
+        case .postRoute:
+            return .post
+        case .getRoute:
+            return .get
+        case .getReview:
+            return .get
+        case .modifyRoute:
+            return .put
+        case .deleteRoute:
+            return .delete
+        case .checkIsRun:
+            return .get
+        case .getAllRoute:
+            return .get
+        case .getAllReview:
+            return .get
+        case .getRouteFromCoordinate:
+            return .get
+        case .getRouteFromCityName:
+            return .get
+        case .checkRouteNameIsDuplicated:
+            return .get
+        case .getRecommendedRouteFromCoordinate:
+            return .get
+        case .getPopularTags:
+            return .get
+        }
+    }
+    
+    var path: String{
+        switch self{
+        case .postRoute:
+            return "/running-route"
+        case .getRoute(let accessToken, let id):
+            return "/running-route/main/\(id)"
+        case .getReview(let accessToken, let id):
+            return "/running-route/sub/\(id)"
+        case .modifyRoute(let accessToken, let id):
+            return "/running-route/\(id)"
+        case .deleteRoute(let accessToken, let id):
+            return "/running-route/\(id)"
+        case .checkIsRun(let accessToken, let id):
+            return "/running-route/checkRunningExperience/\(id)"
+        case .getAllRoute:
+            return "/running-route/allMainRoute"
+        case .getAllReview:
+            return "/running-route/allSubRoute"
+        case .getRouteFromCoordinate(let accessToken, let longitude, let latitude):
+            return "/running-route/searchLocation?latitude=\(latitude)&longitude=\(longitude)"
+        case .getRouteFromCityName:
+            return .get
+        case .checkRouteNameIsDuplicated:
+            return .get
+        case .getRecommendedRouteFromCoordinate:
+            return .get
+        case .getPopularTags:
+            return .get
+        }
+    }
+    
+    var parameters: Alamofire.Parameters?
+    
+    func asURLRequest() throws -> URLRequest {
+        <#code#>
+    }
+    
+    
+}
